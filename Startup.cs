be1962079bd607d53d81c89dd530d7ec7e79172a
dherkyan <dherkyan@gmail.com>
2019-07-30 23:42:36 -0400

    Configurando serviços

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,9 @@ namespace ProAspNetCoreMvcControllersActions
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +28,11 @@ namespace ProAspNetCoreMvcControllersActions
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
