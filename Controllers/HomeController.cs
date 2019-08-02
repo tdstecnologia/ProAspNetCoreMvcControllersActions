cb47731b610c692fcb0a9be3543308225ba23d98
dherkyan <dherkyan@gmail.com>
2019-08-02 09:50:01 -0400

    View Bag e Parametros

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 
 namespace ProAspNetCoreMvcControllersActions.Controllers
 {
@@ -17,5 +15,32 @@ namespace ProAspNetCoreMvcControllersActions.Controllers
         }
 
         public ViewResult Formulario(string nome, string cidade) => View("Resultado", $"{nome} mora em {cidade}");
+
+        public void ReceberFormulario(string nome, string cidade)
+        {
+            Response.StatusCode = 200;
+            Response.ContentType = "text/html";
+            byte[] content = Encoding.ASCII
+            .GetBytes($"<html><body>{nome} vive em {cidade}</body>");
+            Response.Body.WriteAsync(content, 0, content.Length);
+        }
+
+        public ViewResult DataAtual() => View("DataAtual", DateTime.Now);
+
+        public IActionResult Resultado() => View((object)"Mensagem");
+
+        public ViewResult ViewBagParametro()
+        {
+            ViewBag.Mensagem = "Mensagem";
+            ViewBag.Data = DateTime.Now;
+            return View("ViewBag");
+        }
+
+        public RedirectResult Redirecionar()
+        {
+            return Redirect("/home/index");
+        }
+
     }
+
 }
